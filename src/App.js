07e03aa1bfd0cf16c64209e0e5835e9aef145284
arpartch <athena.partch@gmail.com>
2021-04-10 08:26:54 -0700

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./style.css";
+import Nav from "./Nav.js";
 
 
 const App = () => {
   return (
     <div className="App">
       <Nav/>
-      // components are have self closing tags
+      {/* components are have self closing tags */}
       <main className="App-content">
         <span>Empty</span>
       </main>
--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Nave = () => {
+const Nav = () => {
   return (
     <nav className="App-nav">
       <ul>
