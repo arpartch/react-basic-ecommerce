9caa2c9771d2ff18afc1bae0c6f0beca40c364c4
arpartch <athena.partch@gmail.com>
2021-04-10 07:48:46 -0700

    3 files modified

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nave = () => {
+  return (
+    <nav className="App-nav">
+      <ul>
+        <li className="App-nav-item">
+          <button>Items</button>
+        </li>
+        <li className="App-nav-item">
+          <button>Cart</button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
