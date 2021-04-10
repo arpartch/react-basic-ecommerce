6572df3cdb5153bd8f69a3dc2e2d177d1588094d
arpartch <athena.partch@gmail.com>
2021-04-10 08:43:26 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import Nav from "./Nav.js";
 
 
 const App = () => {
+  const [activeTab, setActiveTab] = useState('items');
   return (
     <div className="App">
-      <Nav/>
+      <Nav
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+      />
       {/* components are have self closing tags */}
       <main className="App-content">
-        <span>Empty</span>
+        <Content tab={activeTab}/>
       </main>
     </div>
   );
 }
+
+const Content = ({tab}) => {
+  switch (tab) {
+    case 'items':
+    return <span>the items</span>;
+    case 'cart':
+    return <span>the cart</span>;
+    default:
+      break;
+  }
+};
 
 export default App;
